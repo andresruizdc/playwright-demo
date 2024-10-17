3bddf2c7840ec726967dda34f2f3aca4015f8819
Andres Ruiz <andres.ruiz@oracle.com>
2024-10-17 11:55:18 -0600

    create test case manually

--- tests/generated.spec.ts
+++ tests/generated.spec.ts
@@ -1,15 +1,28 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://demo.playwright.dev/todomvc/#/');
-  await page.getByPlaceholder('What needs to be done?').fill('water the plants');
-  await page.getByPlaceholder('What needs to be done?').press('Enter');
-  await page.getByPlaceholder('What needs to be done?').fill('feed the dog');
-  await page.getByPlaceholder('What needs to be done?').press('Enter');
-  await page.locator('li').filter({ hasText: 'water the plants' }).getByLabel('Toggle Todo').check();
-  await page.getByRole('link', { name: 'Active' }).click();
-  await expect(page.getByTestId('todo-title')).toContainText('feed the dog');
-  await page.getByRole('link', { name: 'Completed' }).click();
-  await expect(page.getByTestId('todo-title')).toContainText('water the plants');
-  await page.getByRole('link', { name: 'Active' }).click();
+test.describe('New Todo', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('');
+  });
+
+  test('test', async ({ page }) => {
+    const todoInput = page.getByPlaceholder('What needs to be done?');
+    await todoInput.fill('water the plants');
+    await todoInput.press('Enter');
+    await todoInput.fill('feed the dog');
+    await todoInput.press('Enter');
+    await page.locator('li').filter({ hasText: 'water the plants' }).getByLabel('Toggle Todo').check();
+    await page.getByRole('link', { name: 'Active' }).click();
+    await expect(page.getByTestId('todo-title')).toContainText('feed the dog');
+    await page.getByRole('link', { name: 'Completed' }).click();
+    await expect(page.getByTestId('todo-title')).toContainText('water the plants');
+    await page.getByRole('link', { name: 'Active' }).click();
+  });
+
+  test('text field is cleared when item is added', async ({ page }) => {
+    const todoInput = page.getByPlaceholder('What needs to be done?');
+    await todoInput.fill('water the plants');
+    await todoInput.press('Enter');
+    await expect(page.getByPlaceholder('What needs to be done?')).toBeEmpty();
+  });
 });
